8428e7fb33cb770d64288c037f60c3f625d251f0
AddH1008 <adrien.hoffet@sunrise.ch>
2023-04-06 09:37:39 +0200

    add libraries

---
+++ testLib.h
@@ -0,0 +1,12 @@
+#ifndef NOTIFICATION_H_
+#define NOTIFICATION_H_
+
+#include "stdint.h"
+
+typedef struct _GPI_MESSAGE_TYPE_ {
+	uint8_t eMessageType1;
+	uint8_t eMessageType2;
+
+} s_Notif_Queue_Message_t;
+
+#endif /* NOTIFICATION_H_ */
